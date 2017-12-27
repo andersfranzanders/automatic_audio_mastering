c207fc06a8b874121e16bf80b8e4a630b2ff2aec
andersfranzanders <franz.anders@gmx.de>
2017-12-27 20:09:07 +0100

    fixed typo

--- controller/Mastering_Pipeline.py
+++ controller/Mastering_Pipeline.py
@@ -18,7 +18,7 @@ def doMastering(y_in, y_ref, sr, parameters):
     y_in_chorus, y_in_start, y_in_end = SP.getLoudestPart(y_in, sr, parameters)
     y_ref_chorus, y_ref_start, y_ref_end = SP.getLoudestPart(y_ref, sr, parameters)
 
-    print("Start: Equalizing Stage")
+    print("Start: Equalizing Stage ---------------------------")
     y_in_filtered = SA.spectralAdaption(y_in, y_in_chorus, y_ref_chorus, parameters)
 
     print("Start: Postprocessing Signal after Filtering for the Compression Stage")
@@ -26,7 +26,7 @@ def doMastering(y_in, y_ref, sr, parameters):
     y_in_chorus_filtered = SP.updateChorusPart(parameters['kernel_length'], y_in_filtered, y_in_start, y_in_end )
 
 
-    print("Start: Compressing Stage")
+    print("Start: Compressing Stage --------------------------")
     y_compressed = DA.dynamicAdaptionDigitized(y_in_filtered, y_in_chorus_filtered, y_ref_chorus, parameters)
     #y_compressed = y_in_filtered
 
